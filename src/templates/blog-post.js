3c61285ffd9ac13e0ed39729d79584af47df378c
Yuta Suzuki <yutasuzuki.r@gmail.com>
2019-12-09 19:28:54 +0900

    nameを修正

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -107,7 +107,6 @@ export const pageQuery = graphql`
       frontmatter {
         date(formatString: "MMMM DD, YYYY")
         title
-        lead
         description
         tags
         featuredimage {
